ebc058b93d8856613d7f22ef4570590ea733d9e9
(Student  C00300670) Lee Lindsay <C00300670@setu.ie>
2023-11-17 10:18:02 +0000

    menu

--- liamleeproject/liamleeproject/FileName.cpp
+++ liamleeproject/liamleeproject/FileName.cpp
@@ -1,5 +1,28 @@
 // Liam Treacy // Lee Lindsay
+
+
+#include <iostream>
+
+
 int main()
 {
+
+
+
 	return 1;
 }
+
+char printMenu() // written by Lee
+{
+	char choice = ' '; // user input choice
+	std::cout << "Please enter a upper case letter to convert from left to right\nand lower to convert from right to left\n\n" << std::endl;
+	std::cout << "[D] Mpg---------<--->---L/100km        [d]" << std::endl;
+	std::cout << "[E] Kilograms---<--->---Stone   & Pounds" << std::endl;
+	std::cout << "[F] Celsius---<--->---Fahrenheit     [f]" << std::endl;
+	std::cout << "[Q] Exit------[---]----Exit           [q]" << std::endl;
+	std::cout << " Enter your choice  : ";
+	std::cin >> choice;
+	return choice;
+}
+
+
